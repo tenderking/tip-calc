0bdf288539bdf15d688b615cd72caa5d7b5e081f
tenderking <gmu003@gmail.com>
2021-12-09 23:40:08 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+
+import CalcContainer from './components/CalcContainer.vue';
+</script>
+
+<template>
+ <h1>Spli<br>tter</h1>
+  <CalcContainer />
+</template>
+
+<style>
+:root{
+
+    --c-cyan: hsl(172, 67%, 45%);
+    
+    /* Neutral */
+    --c-very-dark-cyan: hsl(183, 100%, 15%);
+    --c-Dark-grayish-cyan: hsl(186, 14%, 43%);
+    --c-Dark-grayish-cyan-2: hsl(184, 14%, 56%);
+    --c-Light-grayish-cyan: hsl(185, 41%, 84%);
+    --c-Light-grayish-cyan-2: hsl(189, 41%, 97%);
+    --c-White: hsl(0, 0%, 100%);
+}
+body{
+      font-family: 'Space Mono', monospace;
+  font-size: 24px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: var(--c-Dark-grayish-cyan-2);
+    
+   }
+   h1{
+    color:var(--c-very-dark-cyan);
+    text-align: center;
+    }
+</style>
---
+++ src/components/CalcContainer.vue
@@ -0,0 +1,73 @@
+<template>
+  <main>
+      <section class="left-side">
+<form >
+    <h3>Bill</h3>
+    <input type="text"> 
+    <h3>Select Tip %</h3>
+    <div class="tip-percent-buttons">
+        <button>5%</button>
+        <button>10%</button>
+        <button>15%</button>
+        <button>25%</button>
+        <button>50%</button>
+        <input type="text">
+    </div>
+    <h3>Number of People</h3>
+    <input type="text">
+</form>
+      </section>
+      <section class="right-side">
+<div class="tip-amount">
+    <div class="tip-amount-text"> 
+        <h3>Tip Amount</h3>
+        <p>/ person</p>
+        </div>
+        <h2>{{tipAmount}}</h2>
+
+</div>
+<div class="total-amount">
+    <div class="total-amount-text"> 
+        <h3>Total</h3>
+        <p>/ person</p>
+        </div>
+        <h2>{{total}}</h2>
+        
+</div>
+<div class="reset-btn">
+    <h4>Reset</h4>
+</div>
+      </section>
+  </main>
+</template>
+
+<script lang="ts" setup>
+const total=0.00
+const tipAmount=0.00
+
+</script>
+
+<style>
+
+   main{
+       width: 60rem;
+       height: auto;
+       background-color:var(--c-White);
+       display: flex;
+       border-radius: 10px;
+   }
+   .left-side, .right-side{
+     
+     width:50%;
+     padding: 1em;
+     }
+     h3{
+         color: var(--c-Dark-grayish-cyan);
+     }
+     .right-side{
+         background: var(--c-very-dark-cyan);
+         border-radius: 10px;
+         margin:2rem
+     }
+
+</style>
